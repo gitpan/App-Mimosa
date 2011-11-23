71a04cd5f1c859b2828a1a0e19f5945d422f5c01
Jonathan Leto <jonathan@leto.net>
2011-11-23 21:43:31 +0000

    Import of LETO/App-Mimosa-0.02 from CPAN.
    gitpan-cpan-distribution: App-Mimosa
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LETO/App-Mimosa-0.02.tar.gz
    gitpan-cpan-author:       LETO
    gitpan-cpan-maturity:     released

--- lib/App/Mimosa.pm
+++ lib/App/Mimosa.pm
@@ -28,7 +28,7 @@ use Catalyst ((
 
 extends 'Catalyst';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 $VERSION = eval $VERSION;
 
 # Defaults
@@ -69,18 +69,55 @@ App::Mimosa - Miniature Model Organism Sequence Aligner
 
 =head1 SYNOPSIS
 
-    perl script/app_mimosa_server.pl
+=head2 DEPLOYING
 
-=head1 DESCRIPTION
+For full details on the deploy options:
 
+    perldoc script/mimosa_deploy.pl
+
+To deploy a Mimosa database:
+
+    perl script/mimosa_deploy.pl
+
+=head2 RUNNING
+
+For full details on the server options:
+
+    perldoc script/mimosa_server.pl
+
+To start Mimosa on port 8888:
+
+    perl script/mimosa_server.pl -p 8888
+
+To start Mimosa on the default port (3000):
+
+    perl script/mimosa_server.pl
+
+You should see something like:
+
+    You can connect to your server at http://localhost:3000
+
+Now you can go to L<http://localhost:3000> and try Mimosa!
+
+=head1 DEPENDENCIES
+
+Mimosa has some non-Perl dependencies. Most notably, the NCBI BLAST command-line
+suite of programs. These are detected by attempting to execute
+
+    fastacmd
+
+If it does not exist, then Mimosa will currently not work at all. Future versions
+of Mimosa may detect different alignment programs such as BWA.
+
+The above command is also necessary to run the Mimosa test suite.
 
 =head1 SEE ALSO
 
 L<App::Mimosa::Controller::Root>, L<Catalyst>
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Jonathan "Duke" Leto <jonathan@leto.net>
+Jonathan "Duke" Leto <jonathan@leto.net>, Rob Buels
 
 =head1 LICENSE
 
